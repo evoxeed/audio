dfb71bea5578f72782cd2077e78894118081d97b
evoxeed <nikita.gomozov@gmail.com>
2023-11-13 22:26:52 +0400

    Upgrade audio player with custom controls and styling
    The commit introduces changes to both the audio player's styling and control functionality. It shifts from utilizing the default browser player controls to a custom set of controls, offering a better look and user experience. On the HTML side, instead of using webkit-media-controls, a custom structure has been used for play control, volume control and a navigation bar.
    
    Further, changes in the CSS file allow the audio player to have a distinct and attractive look. JavaScript file changes allow these custom controls to function as intended, this includes playing and pausing a song, adjusting volume and time navigation.
    
    These changes allow for a more aesthetically pleasing user interface and a smoother user experience.

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -18,8 +18,59 @@ const EXT_tracks = [
 window.addEventListener("DOMContentLoaded", function () {
     initializeTracks();
     const audioPlayer = document.getElementsByTagName("audio")[0];
+    const volumeControl = document.getElementById('volumeControl');
+    const playProgress = document.getElementById('progressBar');
+    const currentTimeDisplay = document.getElementById('currentTime');
+    const totalDurationDisplay = document.getElementById('totalDuration');
+    const playBtn = document.getElementById('playBtn');
+
+    playProgress.min = 0;
+    playProgress.value = 0;
+    playProgress.step = 0.01;
+
     audioPlayer.src = audioTracks[trackNumber];
     audioPlayer.volume = 0.4;
+
+    playBtn.addEventListener('click', () => {
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+    });
+
+    audioPlayer.addEventListener('play', () => {
+        playBtn.textContent = 'Пауза';
+    });
+
+    audioPlayer.addEventListener('pause', () => {
+        playBtn.textContent = 'Играть';
+    });
+
+    volumeControl.addEventListener('input', function () {
+        audioPlayer.volume = this.value;
+    }, false);
+
+    audioPlayer.addEventListener('loadedmetadata', function() {
+        if (isNaN(audioPlayer.duration)) {
+            totalDurationDisplay.textContent = "00:00";
+            playProgress.max = 1;
+        } else {
+            totalDurationDisplay.textContent = formatSeconds(audioPlayer.duration);
+            playProgress.max = audioPlayer.duration;
+        }
+        audioPlayer.play();
+    });
+
+    audioPlayer.addEventListener('timeupdate', function() {
+        currentTimeDisplay.textContent = formatSeconds(audioPlayer.currentTime);
+        playProgress.value = audioPlayer.currentTime;
+    });
+
+    playProgress.addEventListener('change', function() {
+        audioPlayer.currentTime = playProgress.value;
+    });
+
     audioPlayer.play()
     audioPlayer.addEventListener("ended", function () {
         stepAudio(trackNumber + 1);
@@ -39,13 +90,23 @@ function stepAudio(N) {
     randomAudioToggle ? randomAudio(N) : nextAudio(N);
 }
 
+function formatSeconds(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds - minutes * 60);
+    return `${pad(minutes)}:${pad(seconds)}`;
+}
+
+function pad(number) {
+    return number < 10 ? '0' + number : '' + number;
+}
+
 function nextAudio() {
     const audioPlayer = document.getElementsByTagName("audio")[0];
     trackNumber = (trackNumber + 1) % audioTracks.length;
     let S = audioTracks[trackNumber];
     if (S !== undefined) {
         audioPlayer.src = S;
-        audioPlayer.play();
+        // audioPlayer.play();
         console.log("МУЗЫКА: " + trackNumber + ") " + audioPlayer.src);
     } else {
         console.log("МУЗЫКА: " + trackNumber + " неопределено!!!");
